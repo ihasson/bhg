8f97be381411d09f534e5766cf3b349d73eda6ea
Izzy <>
2022-09-05 19:48:02 -0700

    merged work done on laptop into main

---
+++ ch2/echo-server/main.go
@@ -0,0 +1,49 @@
+/* Listing 2-11 */
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func echo(conn net.Conn) {
+	defer conn.Close()
+
+	b := make([]byte, 512)
+	for { //infinite loop?//1
+		//Receive
+		size, err := conn.Read(b[0:]) //2
+		if err == io.EOF {
+			log.Println("Client disconnected")
+			break
+		}
+		if err != nil {
+			log.Println("Unexpected error")
+			break
+		}
+		log.Printf("Received %d bytes: %s\n", size, string(b))
+
+		//Send
+		log.Println("Writing data")
+		if _, err := conn.Write(b[0:size]); err != nil {
+			log.Fatalln("Unable to write data")
+		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on 0.0.0.0:20080")
+	for { // another infinite loop?
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go echo(conn)
+	}
+}
